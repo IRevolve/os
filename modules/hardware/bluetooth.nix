af6f108d89cf5b2ee2a9e61ddfd598597f75dd5e
Revolve <revolve@atreia.io>
2026-07-09 22:04:19 +0200

    fix disconnecting bluetooth controller

--- modules/hardware/bluetooth.nix
+++ modules/hardware/bluetooth.nix
@@ -1,5 +1,11 @@
 { self, inputs, ... }: {
   flake.nixosModules.bluetooth = { ... }: {
     hardware.bluetooth.enable = true;
+    hardware.bluetooth.powerOnBoot = true;
+
+    # Fix Xbox-Controller disconnect
+    boot.extraModprobeConfig = ''
+      options bluetooth disable_etrm=1
+    '';
   };
 }
