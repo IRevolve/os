75a2e1273200a665cf6c5a4c4441a69c2588904c
Revolve <revolve@atreia.io>
2026-07-10 22:47:57 +0200

    add cursor

--- modules/features/desktop.nix
+++ modules/features/desktop.nix
@@ -27,5 +27,22 @@
       pkgs.xwayland-satellite
     ];
 
+    home-manager.users.revolve = {
+      home.pointerCursor = {
+        enable = true;
+        gtk.enable = true;
+        x11.enable = true;
+        package = pkgs.apple-cursor;
+        name = "macOS";
+        size = 24;
+      };
+    };
+
+    environment.variables = {
+      XCURSOR_SIZE = "24";
+      XCURSOR_THEME = "macOS";
+      GTK_CURSOR_SIZE = "24";
+      GTK_CURSOR_THEME = "macOS";
+    };
   };
 }
